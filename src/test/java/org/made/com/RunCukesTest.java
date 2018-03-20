58e192ed399963c6ffff73c767573a3c617ba2b9
bhavana2995 <kodumurubhavana@gmail.com>
2018-03-20 20:36:51 +0000

    remove comments

--- src/test/java/org/made/com/RunCukesTest.java
+++ src/test/java/org/made/com/RunCukesTest.java
@@ -4,9 +4,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-/**
- * this is sample code
- */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources", dryRun = false)
 public class RunCukesTest {
